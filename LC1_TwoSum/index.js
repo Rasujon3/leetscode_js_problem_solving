aae5d276ae5b661060fb3d16cbb0eb58d4d68638
Ruhul Amin Sujon <rasujon3@gmail.com>
2021-12-19 22:09:48 +0600

    prblm-1 solve

--- LC1_TwoSum/index.js
+++ LC1_TwoSum/index.js
@@ -6,6 +6,15 @@
 // twoSum([2,7,11,15], 9) --> [0,1]
 // twoSum([3,5,4], 9) --> [1,2]
 
-function twoSum(arr, target) {}
+function twoSum(nums, target) {
+    for (let index = 0; index < nums.length; index++) {
+        const diff = target - nums[index];
+        const diffIndex = nums.indexOf(diff);
+        // "diffIndex !== index" takes care of same index not being reused
+        if (diffIndex !== -1 && diffIndex !== index) {
+            return [index, diffIndex];
+        }
+    }
+}
 
 module.exports = twoSum;
