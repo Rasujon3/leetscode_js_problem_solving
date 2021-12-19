2fd3a7148d832a8718933333cd7c27512ad5094f
Ruhul Amin Sujon <rasujon3@gmail.com>
2021-12-20 02:36:17 +0600

    module-2

---
+++ LC20_isValid/index.js
@@ -0,0 +1,16 @@
+const isValid = s => {
+    let stack = [];
+    let pairsHashMap = {"(":")", "{":"}", "[":"]"};
+
+    for (let i = 0; i < s.length; i++) {
+        const char = s[i];
+        if (pairsHashMap[char]) {
+            stack.push(char);
+        } else if(pairsHashMap[stack.pop()] !== char) {
+            return false;
+        }
+    }
+    return stack.length === 0;
+};
+
+module.exports = isValid;
